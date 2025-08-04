04988f108dc0c8ba4e6b005894d0160f696fad4d
marthag0311 <marageofrey@gmail.com>
2025-08-04 23:50:13 +0200

    Add files via upload

---
+++ 00 Practise/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace _00_Practise
+{
+    internal class Program
+    {
+        //static string[] 
+        
+        static void Main(string[] args)
+        {
+            /*string question = Console.ReadLine();
+
+            switch (question)
+            {
+                case "B1":
+
+                    break;
+                case "B2":
+                    break;
+                case "B3":
+                    break;
+                default:
+                    Console.WriteLine("crazy input");
+                    break;
+            }*/
+
+            /*
+             Read in a file and find the most used smallest 
+             word (at least 2 characters). Make your program 
+             as bulletproof as possible, otherwise, print 'crazy input'
+
+            input:
+            MaxHavelaar.txt
+
+            output:
+            ik
+
+            input:
+            LoremIpsum.txt
+
+            output:
+            in
+             */
+
+            string phrase = Console.ReadLine().ToUpper();
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //double number = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //char character = Convert.ToChar(Console.ReadLine().ToLower());
+            //char.isDigit(character)
+            //char.isLetter(character)
+
+            
+
+
+        }
+    }
+
+}
---
+++ 00 Practise/QuestionB3.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_Practise
+{
+    class QuestionB3
+    {
+    }
+}
