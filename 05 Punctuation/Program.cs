181e24cd4adf2487096d8df2b6c0837e82ff01aa
marthag0311 <marageofrey@gmail.com>
2025-08-05 06:32:27 +0200

    Add files via upload

---
+++ 05 Punctuation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _05_Punctuation
+{
+    /*
+        Read a sentence and correct the punctuation! 
+        Replace symbols that occur multiple times with 
+        just one instance of the same symbol! We consider 
+        the following symbols as as punctuation symbols: 
+        full stops (.), commas (,), question marks (?), 
+        exclamation marks (!), colons (:),  hyphen (-), 
+        parentheses ( () ), braces ({}), brackets ([]), 
+        semi-colons (;), apostrophes (') and speech marks ("). 
+
+        Important: use of Linq-library is forbidden!
+
+
+        Input/Output
+        input:
+        Don'''t put all your eggs in one basket!!!
+        output:
+        Don't put all your eggs in one basket!
+
+        input:
+        Every cloud has a silver lining....
+        output:
+        Every cloud has a silver lining.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+
+            string phrase = "";
+            char temporary = 'a';
+
+
+            foreach (char character in sentence)
+            {
+                if (character == '.' || character == ',' || character == '?' ||
+                    character == '!' || character == ':' || character == '-' ||
+                    character == '(' || character == ')' || character == '{' ||
+                    character == '}' || character == '[' || character == ']' ||
+                    character == ';' || character == '\'' || character == '\"')
+                {
+                    if (character != temporary)
+                    {
+                        phrase += character;
+                        temporary = character;
+                    }
+                }
+                else
+                {
+                    phrase += character;
+                }
+            }
+            Console.WriteLine(phrase);
+        }
+    }
+}
---
+++ 05 Single Ladies/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace _05_Single_Ladies
+{
+    /*
+        Read in a filename. Find all single 'ladies' (also women & girls ;)) in a given file (files are already in CodeGrade and are located in same folder). Download files.Download Download files.
+
+        Important: use of Linq-library is forbidden!
+
+        input:
+        Beyonce.txt
+
+        output:
+        girl/girls: 953 matches found
+        lady/ladies: 291 matches found
+        woman/women: 203 matches found
+
+        input:
+        LadyGaga.txt
+
+        output:
+        girl/girls: 366 matches found
+        lady/ladies: 849 matches found
+        woman/women: 51 matches found
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = Console.ReadLine();
+
+            StreamReader read = new StreamReader(filename);
+            string text = read.ReadToEnd().ToLower();
+
+            int girlORgirls = 0; 
+            int ladyORladies = 0;
+            int womanORwomen = 0;
+            string single = "";
+
+            foreach (char character in text)
+            {
+                if (character >= 'a' && character <= 'z')
+                {
+                    single += character;
+                }
+                else
+                {
+                    if (single == "girl" || single == "girls")
+                    {
+                        girlORgirls++;
+                    }
+                    else if (single == "lady" || single == "ladies")
+                    {
+                        ladyORladies++;
+                    }
+                    else if (single == "woman" || single == "women")
+                    {
+                        womanORwomen++;
+                    }
+                    single = "";
+                }
+            }
+            Console.WriteLine("girl/girls: " + girlORgirls + " matches found");
+            Console.WriteLine("lady/ladies: " + ladyORladies + " matches found");
+            Console.WriteLine("woman/women: " + womanORwomen + " matches found");
+            read.Close();
+        }
+    }
+}
