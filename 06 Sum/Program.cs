5de8aa7351d86429a5922f05cb269c7e3e533b73
marthag0311 <marageofrey@gmail.com>
2025-08-05 06:41:21 +0200

    Add files via upload

---
+++ 06 Sum/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _06_Sum
+{
+    internal class Program
+    {
+        /*
+            Read in a file name and find all the numbers in that 
+            file and add them up. Important, exclude the numbers 
+            from years (eg 2018) and dates (eg 2018-09-10)! 
+
+            Note: files files.zip Download files.zipare already 
+            in CodeGrade and use of Linq-library or Array-library 
+            is forbidden!
+
+            input:
+            Beyonce.txt
+
+            output:
+            10877
+
+            input:
+            LadyGaga.txt
+
+            output:
+            3743
+         */
+        static void Main(string[] args)
+        {
+            string filename = Console.ReadLine();
+                                 
+            StreamReader read = new StreamReader(filename);
+            string line = read.ReadLine();
+
+            int sum = 0;
+
+            while (line != null)
+            {
+                string[] phrases = line.Split(";");
+
+                Regex dates = new Regex(@"\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])*");
+
+                Regex years = new Regex(@"^(19|20)[0-9][0-9]");
+
+                Regex regex = new Regex(@"\b\d+\b");
+
+                foreach (string phrase in phrases)
+                {                    
+                    if (!dates.IsMatch(phrase) && !years.IsMatch(phrase))
+                    {
+                        MatchCollection matches = regex.Matches(phrase);
+
+                        foreach (Match match in matches)
+                        {
+                            if (!years.IsMatch(match.Value))
+                            {
+                                if (Int32.TryParse(match.Value, out int number))
+                                {
+                                    sum += number;
+                                }
+                            }
+                        }
+                    }
+                }
+                line = read.ReadLine();
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 07 Caesar/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+
+namespace _07_Caesar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            try
+            {
+                string input = Console.ReadLine();
+                string message = Console.ReadLine().ToUpper();
+
+                bool isAlphabet = true;
+
+                foreach (char character in message)
+                {
+                    if (character >= 'A' && character <= 'Z')
+                    {
+                        isAlphabet = true;
+                    }
+                    else
+                    {
+                        isAlphabet = false;
+                        break;
+                    }
+                }
+
+                char[] alphabet = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                'W', 'X', 'Y', 'Z'};
+
+                if (isAlphabet)
+                {
+
+                    if (Int32.TryParse(input, out int shift))
+                    {
+                        string encrypted = "";
+
+                        for (int i = 0; i < message.Length; i++)
+                        {
+                            //find the position of the letter in the alphabet array
+                            int oldposition = Array.IndexOf(alphabet, message[i]);
+
+                            //add/minus to find the new position
+                            int newPosition = (oldposition + shift) % 26;
+
+                            if (newPosition < 0)
+                            {
+                                //get the new letter in the alphabet array with the new position
+                                char newLetter = alphabet[alphabet.Length + newPosition];
+
+                                //add the encrypted letter to the encrypted string
+                                encrypted += newLetter;
+                            }
+                            else
+                            {
+                                //get the new letter in the alphabet array with the new position
+                                char newLetter = alphabet[newPosition];
+
+                                //add the encrypted letter to the encrypted string
+                                encrypted += newLetter;
+                            }
+                        }
+                        Console.WriteLine(encrypted);
+                    }
+                    else
+                    {
+                        Console.WriteLine("converting problem");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("crazy input");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("converting problem");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("too big for this type");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (Exception) //(Exception ex)
+            {
+                Console.WriteLine("crazy input");
+                //Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
