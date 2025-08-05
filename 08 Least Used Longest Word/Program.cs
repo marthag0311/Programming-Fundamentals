b6d7d7063139b695f86345898cfd13cd617fd4db
marthag0311 <marageofrey@gmail.com>
2025-08-05 06:50:18 +0200

    Add files via upload

---
+++ 08 Least Used Longest Word/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _08_Least_Used_Longest_Word
+{
+    /*
+        Read in a file (download files: Boeken.zip 
+        Download Boeken.zip, files already in CodeGrade)  
+        and find the least used longest word. Make your 
+        program as bulletproof as possible, otherwise, 
+        print 'crazy input'
+
+        input:
+        MaxHavelaar.txt
+        output:
+        marmerknikkers
+
+        input:
+        LoremIpsum.txt
+        output:
+        lsuspendisse
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filename = Console.ReadLine();
+
+                StreamReader read = new StreamReader(filename);
+                string text = read.ReadToEnd().ToLower();
+
+                string temp_word = "";
+                Dictionary<string, int> words = new Dictionary<string, int>();
+
+                foreach (char character in text)
+                {
+                    if (character >= 'a' && character <= 'z')
+                    {
+                        temp_word += character;
+                    }
+                    else
+                    {
+                        if (!words.ContainsKey(temp_word))
+                        {
+                            words.Add(temp_word, 1);
+                        }
+                        else
+                        {
+                            words[temp_word]++;
+                        }
+                        temp_word = "";
+                    }
+                }
+
+                //int max = 99999999;
+                string word = "";
+
+                foreach (var pair in words)
+                {
+                    if (pair.Key.Length > word.Length && pair.Value == 1)
+                    {
+                        word = pair.Key;
+                        //max = pair.Value;
+                    }
+                }
+                Console.WriteLine(word);
+
+                read.Close();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (Exception) //(Exception ex)
+            {
+                Console.WriteLine("crazy input");
+                //Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ 08 Longest Isogram/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _08_Longest_Isogram
+{
+    /*
+        A word in which no letter of the alphabet occurs 
+        more than once is an isogram. Now read in a file 
+        (download files: Boeken.zip Download Boeken.zip, 
+        files already in CodeGrade)  and find the longest 
+        isogram. Make your program as bulletproof as possible, 
+        otherwise, print 'crazy input'
+
+        input:
+        MaxHavelaar.txt
+        output:
+        uitmaken
+
+        input:
+        LoremIpsum.txt
+        output:
+        interdum
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filename = Console.ReadLine();
+
+                StreamReader read = new StreamReader(filename);
+                string text = read.ReadToEnd().ToLower();
+
+                string word = "";
+                string temp_word = "";
+                bool isOne = true;
+                Dictionary<char, int> characters = new Dictionary<char, int>();
+
+                foreach (char character in text)
+                {
+                    if (character >= 'a' && character <= 'z')
+                    {
+                        temp_word += character;
+                    }
+                    else
+                    {
+                        if (word.Length < temp_word.Length)
+                        {
+                            foreach (char element in temp_word)
+                            {
+                                if (!characters.ContainsKey(element))
+                                {
+                                    characters.Add(element, 1);
+                                }
+                                else
+                                {
+                                    characters[element]++;
+                                }
+                            }
+
+                            foreach (var pair in characters)
+                            {
+                                if (pair.Value > 1)
+                                {
+                                    isOne = false;
+                                    break;
+                                }
+                                else
+                                {
+                                    isOne = true;
+                                }
+                            }
+
+                            if (isOne)
+                            {
+                                word = temp_word;
+                            }
+
+                            foreach (var key in characters.Keys)
+                            {
+                                characters.Remove(key);
+                            }
+                        }
+                        temp_word = "";
+                    }
+                }
+                Console.WriteLine(word);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("crazy input");
+            }
+            catch (Exception) //(Exception ex)
+            {
+                Console.WriteLine("crazy input");
+                //Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
