9b3db186a7a6be425127e35f6731d27a72f4b9e7
marthag0311 <marageofrey@gmail.com>
2025-08-05 06:35:16 +0200

    Add files via upload

---
+++ 05 Z of Zorro/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace _05_Z_of_Zorro
+{
+    /*
+        Apparently, the character Z is the least used in the English 
+        language, so find all (unique!) lowercase words from a given 
+        file containing a Z. Print them out in the order they appear 
+        in the file (files are already in CodeGrade and are located 
+        in same folder)! Download files.Download Download files.
+
+        Important: use of Linq-library is forbidden!
+
+        input:
+        BillieEilish.txt
+
+        output:
+        froze vandalize size amazing fantasize sympathize frozen 
+        arizona mnoppuzzle zeds gazzo paparazzi hazey iizi kez 
+        bozar crazy 
+
+        input:
+        SelenaGomez.txt
+
+        output:
+        gomez sympathize size zipper crazy hazy recognize fantasize 
+        mesmerized vez realize wizards breeze amazing crazier 
+        crcrcrazier jazz hypnotized civilized lazy corazon empieza 
+        razona frozen zapped dizzy cuz akzel 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = Console.ReadLine();
+
+            StreamReader read = new StreamReader(filename);
+            string text = read.ReadToEnd().ToLower();
+
+            Regex regex = new Regex(@"[^\s]*[z][^\s]*");
+            MatchCollection matches = regex.Matches(text);
+
+            string phrase = "";
+
+            foreach (Match match in matches)
+            {
+                if (!phrase.Contains(match.Value))
+                {
+                    phrase += match.Value + " ";
+                }
+            }
+
+            string words = "";
+            bool lettersOnly = true;
+
+            string[] array = phrase.Split(" ");
+
+            foreach (string word in array)
+            {
+                foreach (char element in word)
+                {
+                    if (char.IsDigit(element))
+                    {
+                        lettersOnly = false;
+                        break;
+                    }
+                    else
+                    {
+                        lettersOnly = true;
+                    }
+                }
+
+                if (lettersOnly)
+                {
+                    words += word + " ";
+                }
+            }
+
+            string temporary = "";
+            string noSymbols = "";
+
+            foreach (char character in words)
+            {
+                if (char.IsLetter(character))
+                {
+                    temporary += character;
+                }
+                else
+                {
+                    if (temporary.Contains('z'))
+                    {
+                        if (!noSymbols.Contains(temporary))
+                        {
+                            noSymbols += temporary + " ";
+                        }
+                    }
+                    temporary = "";
+                }
+            }
+
+            string alphabetOnly = "";
+            string[] unique = noSymbols.Trim().Split(" ");
+
+            foreach (string word in unique)
+            {
+                if (Regex.IsMatch(word, @"^[a-zA-Z]+$"))
+                {
+                    alphabetOnly += word + " ";
+                }
+            }
+            Console.WriteLine(alphabetOnly.Trim());            
+        }
+    }
+}
---
+++ 06 Anagram/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace _06_Anagram
+{
+    /*
+        Read in two strings and find out if they are anagrams of each other. 
+        An anagram is a word or phrase formed by rearranging the letters of 
+        a different word or phrase, for example, the word binary can be 
+        rearranged into brainy.
+
+        Note: use of Linq-library or Array-library is forbidden!
+
+        input:
+        evil
+        vile
+
+        output:
+        True
+
+        input:
+        forty five
+        fifty four
+
+        output:
+        False
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string read = Console.ReadLine().ToLower().Replace(" ", "");
+            string input = Console.ReadLine().ToLower().Replace(" ", "");
+
+            char[] phrase = read.ToCharArray();
+            char[] sentence = input.ToCharArray();
+
+            //Sorting
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                for (int j = i; j < phrase.Length; j++)
+                {
+                    if (phrase[j] < phrase[i])
+                    {
+                        char temporary = phrase[i];
+                        phrase[i] = phrase[j];
+                        phrase[j] = temporary;
+                    }
+                }
+            }
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                for (int j = i; j < sentence.Length; j++)
+                {
+                    if (sentence[j] < sentence[i])
+                    {
+                        char temporary = sentence[i];
+                        sentence[i] = sentence[j];
+                        sentence[j] = temporary;
+                    }
+                }
+            }
+
+            //Anagram?   
+            string word = string.Join("", phrase);
+            string term = string.Join("", sentence);
+
+            if (word == term)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }          
+        }
+    }
+}
